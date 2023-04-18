d214d5fdef15d543d07e4d2127ff20f12a8b1513
sarahPC <sarahapplebaum.sia@gmail.com>
2023-04-18 15:34:57 -0400

    stuff

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Slider healthSlider;
+
+    bool gameOver;
+
+    private int _health;
+    public int Health{
+        get{
+            return _health;
+        }
+        set{
+            _health = value;
+            if (_health <= 0)
+            {
+                
+            }
+        }
+    }
+    private void Awake() {
+        
+    }
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
